6df273770ec55c06c1f03d29a7dbbf1bb2b6de35
이동주 <moovinzoo@idongjuui-macbook-pro.local>
2018-11-19 19:07:25 +0900

    Initial Commit

---
+++ rppg_181119/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rppg_181119
+//
+//  Created by 이동주 on 2018. 11. 19..
+//  Copyright © 2018년 이동주. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

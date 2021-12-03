a281d4ac74a9abea81abfc6ae0fc3259ebfb9bd3
moovinzoo <moovinzoo@gmail.com>
2021-12-03 17:37:31 +0900

    [UPDATE] missing bodies

--- rppg_181119/main.cpp
+++ rppg_181119/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  rppg_181119
-//
-//  Created by 이동주 on 2018. 11. 19..
-//  Copyright © 2018년 이동주. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
